cd7e7076430422ae53cfed70189672330eebde08
Audric Rosario <audricarr050@gmail.com>
2020-03-09 14:22:22 -0400

    Ultima actualzacion
    09 03 2020

---
+++ Actividad 2/clase 1/Assets/Scripts/Cannon/ParabolicPredictions.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+public class ParabolicPredictions : MonoBehaviour
+{
+    LineRenderer ir;
+
+    private float velocity;
+    private float angle;
+    public int resolution = 20;
+    CannonController playerRoute;
+    float g; // fuerza de gravedad
+             // Start is called before the first frame update
+    float radAngle;
+    private void Awake()
+    {
+        ir = GetComponent<LineRenderer>();
+        g = Mathf.Abs(Physics.gravity.y);
+        ir.positionCount = 2 * resolution + 1;
+    }
+
+    
+    void Start()
+    {
+        playerRoute = gameObject.GetComponent<CannonController>();
+    }
+
+    //[System.Obsolete]
+    void Update()
+    {
+        velocity = playerRoute.triggerSpeed * playerRoute.power;
+        angle = playerRoute.triggerAngle * Mathf.Rad2Deg;
+        
+        RenderArc();
+
+    }
+
+    //[System.Obsolete]
+    void RenderArc()
+    {
+        
+        //ir.SetVertexCount(resolution + 1);
+        ir.SetPositions(CalculateArcArray());
+    }
+
+    Vector3[] CalculateArcArray()
+    {
+        Vector3[] arcArray = new Vector3[2*resolution + 1];
+        radAngle = Mathf.Deg2Rad * angle;
+        float absDistance = (velocity * velocity * Mathf.Sin(2 * radAngle)) / g;
+        
+        for (int i = 0; i <= resolution; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, absDistance);
+        }
+
+        for (int i = resolution + 1; i <= resolution*2; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, absDistance);
+        }
+        return arcArray;
+    }
+
+    Vector3 CalculateArcPoint(float t, float radDistance)
+    {
+        float x = t * radDistance;
+        float y = x * Mathf.Tan(radAngle) - (g * x * x) / (2 * velocity * velocity * Mathf.Cos(radAngle) * Mathf.Cos(radAngle));
+        return new Vector3(x, y);
+    }
+
+}
---
+++ Tarea 2/Assets/Scripts/ParabolicThrowTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParabolicThrowTest : MonoBehaviour
+{
+    private Vector3 _mouse;
+    public Animator attackMotions;
+    private Camera mainCamera;
+    public GameObject ProjectilePrefab;
+    private GameObject _projectile;
+    public float xVelo = 1f, yVelo = 1f;
+    // Variables con tiro parabolico actualizado
+    public float timeTillHit = 1f;
+    private bool canAttack;
+    
+    /*
+     * Variables de tiro parabolico
+    public float heightOffset;
+    public float arrowSpeed = 1.0f;
+    public float gravity = 9.81f;*/
+
+
+    void Start() {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _mouse = new Vector3();
+        canAttack = true;
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1") && canAttack)
+        {
+            StartCoroutine("Attack");
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        canAttack = false;
+        _mouse = Input.mousePosition;
+        _mouse = mainCamera.ScreenToWorldPoint(_mouse);
+        float xdistance = _mouse.x - gameObject.transform.position.x;
+        float ydistance = _mouse.y - gameObject.transform.position.y;
+
+        // Forma vieja de hacerlo
+        float throwAngle = Mathf.Atan((ydistance + 4.905f)/xdistance); 
+        //float throwAngle = Mathf.Atan((ydistance + 4.905f * (timeTillHit * timeTillHit)) / xdistance);
+        // Forma vieja de hacerlo.
+        float totalVelo = xdistance / Mathf.Cos(throwAngle);
+        //float totalVelo = xdistance / (Mathf.Cos(throwAngle) * timeTillHit);
+        float xVelo, yVelo;
+        xVelo = totalVelo * Mathf.Cos(throwAngle);
+        yVelo = totalVelo * Mathf.Sin(throwAngle);
+
+        attackMotions.SetBool("isAttacking", true);
+        yield return new WaitForSeconds(0.2f);
+        _projectile = Instantiate(ProjectilePrefab, gameObject.transform.position,
+                        Quaternion.Euler(new Vector3(0, 0, throwAngle * Mathf.Rad2Deg)));
+        _projectile.GetComponent<Rigidbody>().velocity = new Vector3(xVelo, yVelo, 0);
+        attackMotions.SetBool("isAttacking", false);
+        yield return new WaitForSeconds(0.8f);
+
+        canAttack = true;
+    }
+    
+}
